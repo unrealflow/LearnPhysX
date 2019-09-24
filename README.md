a07ebee56eab5ff0256b140cd5cd9f25f0ea6e34
unrealflow <867621300@qq.com>
2019-09-24 09:31:42 +0800

    Base Framework

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # LearnPhysX
+
+## Requirements:
+* bin\glut32.dll
+* bin\PhysX_64.dll
+* bin\PhysXCommon_64.dll
+* bin\PhysXCooking_64.dll
+* bin\PhysXDevice64.dll
+* bin\PhysXFoundation_64.dll
+* bin\PhysXGpu_64.dll
---
+++ base/SkUtils.h
@@ -0,0 +1,49 @@
+﻿#include "PxPhysicsAPI.h"
+#include "string"
+
+class SkErrorCallback : public physx::PxErrorCallback
+{
+public:
+    virtual void reportError(physx::PxErrorCode::Enum code, const char *message, const char *file,
+                             int line)
+    {
+        if (code < 0)
+        {
+            return;
+        }
+        std::string type = "";
+        switch (code)
+        {
+        case physx::PxErrorCode::eNO_ERROR:
+            type = "NO_ERROR";
+            break;
+        case physx::PxErrorCode::eDEBUG_INFO:
+            type = "DEBUG_INFO";
+            break;
+        case physx::PxErrorCode::eDEBUG_WARNING:
+            type = "DEBUG_WARNING";
+            break;
+        case physx::PxErrorCode::eINVALID_PARAMETER:
+            type = "INVALID_PARAMETER";
+            break;
+        case physx::PxErrorCode::eINVALID_OPERATION:
+            type = "INVALID_OPERATION";
+            break;
+        case physx::PxErrorCode::eOUT_OF_MEMORY:
+            type = "OUT_OF_MEMORY";
+            break;
+        case physx::PxErrorCode::eINTERNAL_ERROR:
+            type = "INTERNAL_ERROR";
+            break;
+        case physx::PxErrorCode::eABORT:
+            type = "ABORT";
+            break;
+        case physx::PxErrorCode::ePERF_WARNING:
+            type = "PERF_WARNING";
+            break;
+        default:
+            break;
+        }
+        fprintf(stderr,"DEBUG %s in %s,%d: %s...\n",type.c_str(),file,line,message);
+    }
+};
